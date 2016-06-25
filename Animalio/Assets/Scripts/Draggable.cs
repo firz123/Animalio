f5f61c90f7ccf35c902fddf209e6c5e279e7d15e
Imaani Choudhuri <imaanirc@gmail.com>
2016-06-25 09:13:28 -0700

    Adding the drag/drop scripts to a new branch because I need to get the new Food Web GUI

--- Animalio/Assets/Scripts/Draggable.cs
+++ Animalio/Assets/Scripts/Draggable.cs
@@ -2,9 +2,42 @@
 using UnityEngine.EventSystems;
 using System.Collections;
 
-public class Draggable : MonoBehaviour, IDragHandler {
+public class Draggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler {
+    public static GameObject itemBeingDragged;
+    Vector3 startposition;
+    Transform startparent; 
+    
 
-    public void OnDrag( PointerEventData eventData ) { 
-    transform.position = Input.mousePosition; 
+    #region IBeginDragHandler Members
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        startparent = transform.parent; 
+        startposition = transform.position; 
+        itemBeingDragged = gameObject;
+        GetComponent<CanvasGroup>().blocksRaycasts = false;
     }
+
+    #endregion
+
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        transform.position = Input.mousePosition;
+    }
+
+    #region IEndDragHandler Members
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        itemBeingDragged = null;
+        if (transform.parent != startparent)
+        {
+            transform.position = startposition;
+        }
+        Debug.Log(transform.parent.gameObject.name);
+        GetComponent<CanvasGroup>().blocksRaycasts = true;
+    }
+
+    #endregion
 }
---
+++ Animalio/Assets/Scripts/Droppable.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+public class Droppable : MonoBehaviour, IDropHandler  {
+    public GameObject item
+    { get {
+        if(transform.childCount > 0)
+        {return transform.GetChild (0).gameObject
+            ; }
+        return null;
+        }
+        
+        set{}
+        
+    }
+
+    #region IDropHandler Members
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        if (!item)
+            {
+        Draggable.itemBeingDragged.transform.SetParent(transform); 
+        Debug.Log("dropped"); 
+         }
+
+    }
+
+    #endregion
+}
